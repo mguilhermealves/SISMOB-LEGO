e7b63188d6ff37352d5c5c69dd60442c35924e72
Marcos Alves <malves@hsolmkt.com.br>
2022-04-29 00:21:26 -0300

    ajuste

--- gerenciador/cgi-bin/atualizar_status_gerencianet.php
+++ gerenciador/cgi-bin/atualizar_status_gerencianet.php
@@ -27,29 +27,31 @@ $clientId = 'Client_Id_8f31bad8f7b617e1dd8c3f90b004b3a8bae64ffe'; // informe seu
 $clientSecret = 'Client_Secret_0c6477c6f0e9bc98d107f99a68c428c6b8f5e4ea'; // informe seu Client_Secret
 
 $payment = new payments_model();
-$payment->set_filter(array(" active = 'yes' "));
+$payment->set_filter(array(" active = 'yes' and payment_method = 'ticket'"));
 $payment->load_data();
-$data = current($payment->data);
+$data = $payment->data;
 
 if (!empty($data)) {
-    $options = [
-        'client_id' => $clientId,
-        'client_secret' => $clientSecret,
-        'sandbox' => true, // altere conforme o ambiente (true = desenvolvimento e false = produção)
-        'timeout' => 10000
-    ];
+    foreach ($data as $k => $v) {
+        $options = [
+            'client_id' => $clientId,
+            'client_secret' => $clientSecret,
+            'sandbox' => true, // altere conforme o ambiente (true = desenvolvimento e false = produção)
+            'timeout' => 10000
+        ];
 
-    // $charge_id refere-se ao ID da transação ("charge_id")
-    $params = [
-        'id' => $data["charge_id"]
-    ];
+        // $charge_id refere-se ao ID da transação ("charge_id")
+        $params = [
+            'id' => $v["charge_id"]
+        ];
 
-    $api = new Gerencianet($options);
-    $charge = $api->detailCharge($params, []);
+        $api = new Gerencianet($options);
+        $charge = $api->detailCharge($params, []);
 
-    $info["post"]["status"] = $charge["data"]["status"];
-    $info["post"]["historic_bank"] = serialize($charge["data"]["history"]);
-
-    $payment->populate($info["post"]);
-    $payment->save();
+        $info["post"]["status"] = $charge["data"]["status"];
+        $info["post"]["historic_bank"] = serialize($charge["data"]["history"]);
+        
+        $payment->populate($info["post"]);
+        $payment->save();
+    }
 }
