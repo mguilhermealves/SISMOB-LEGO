8d028e462c1d38292f2516775e3adfaa1ad7eb33
Marcos Alves <malves@hsolmkt.com.br>
2022-06-28 15:05:54 -0300

    ajuste

--- gerenciador/app/inc/model/users_model.php
+++ gerenciador/app/inc/model/users_model.php
@@ -1,6 +1,6 @@
 <?php
 class users_model extends DOLModel {
-	protected $field = array( " idx " , " enabled ", " first_name " , " last_name " , " cpf " , " mail " , " marital_status ", " login " , " last_login " , " phone " , " celphone " , " genre " , " rg ", " cnh ", " postalcode ", " address ", " number ", " complement ", " district ", " city " , " uf " , " marital_status ", " created_at " ) ;
+	protected $field = array( " idx " , " first_name " , " last_name " , " cpf " , " mail " , " marital_status ", " login " , " last_login " , " phone " , " celphone " , " genre " , " rg ", " cnh ", " postalcode ", " address ", " number ", " complement ", " district ", " city " , " uf " , " marital_status ", " created_at " ) ;
 	protected $filter = array( " active = 'yes' " ) ;
 	function __construct( $bd = false  ) {
 		return parent::__construct( "users" , $bd );
